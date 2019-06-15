8ebc811a702c5f3a9d75aa5077fdd86fff80e523
nikkolasg <nikkolasg@gmail.com>
2019-06-15 11:34:37 -0400

    updated sloth circuit array

--- vdf/vdf.js
+++ vdf/vdf.js
@@ -3,7 +3,8 @@ const BN128 = require('snarkjs').bn128
 const toStr = stringifyBigInts
 
 // Assum x, k are BigInt
-const SLOTH_P = BN128.r
+//const SLOTH_P = BN128.r
+const SLOTH_P = bigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617");
 const SLOTH_V = bigInt('17510594297471420177797124596205820070838691520332827474958563349260646796493')
 function sloth_encode (x, k, iter) {
   for (let i = 0; i < iter; i++) {
@@ -38,9 +39,10 @@ function witness (c, k, x) {
 function test () {
   const x = bigInt('12345')
   const k = bigInt('99999')
+  const iter = 2;
 
-  const c = sloth_encode(x, k, 1)
-  const d = sloth_decode(c, k, 1)
+  const c = sloth_encode(x, k, iter)
+  const d = sloth_decode(c, k, iter)
   console.log({x, c, k, d}, d.equals(x))
   witness(c, k, x)
 
